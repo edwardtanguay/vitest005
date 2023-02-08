fa0351f4ebb1d30f4feec627bccfd5edc761da63
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-08 10:10:32 +0100

    app removed from server

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as model from './model.js';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send(model.getApiInstructions());
+});
+
+app.get('/books', (req: express.Request, res: express.Response) => {
+	res.json(model.getBooks());
+});
+
+app.get('/books/:id', (req: express.Request, res: express.Response) => {
+	const id = Number(req.params.id);
+	if (isNaN(id)) {
+		res.status(400).send({
+			error: true,
+			message: "sent string, should be number"
+		});
+	} else {
+		const book = model.getBook(id);
+		if (book === undefined) {
+			res.status(404).send({
+				error: true,
+				message: "id did not correspond to an existing item"
+			});
+		} else {
+			res.json(book);
+		}
+	}
+});
+
+export default app;
