f39deec10c5ea6fdb5ffc74d8271392ef6a60686
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-08 12:01:11 +0100

    everything works - two test

--- server.test.ts
+++ server.test.ts
@@ -2,8 +2,18 @@ import { test, expect } from 'vitest';
 import request from 'supertest';
 import app from './src/app.js';
 
-test('if the base route works as expected', async () => {
+test('the base route works as expected', async () => {
 	const response = await request(app).get('/');
 	expect(response.statusCode).toBe(200);
 	expect(response.text.length).toBeGreaterThan(250);
+});
+
+test('the /books route works', async () => {
+	const response = await request(app).get('/books');
+	expect(response.statusCode).toBe(200);
+});
+
+test('the /books route returns all books', async () => {
+	const response = await request(app).get('/books');
+	expect(JSON.parse(response.text).length).toBe(35);
 });
