17d524cb78224161bafa9007079320336791f0d9
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-08 11:21:29 +0100

    vitest tests work again in root

--- server.test.ts
+++ server.test.ts
@@ -1,8 +1,8 @@
 import { test, expect } from 'vitest';
 import request from 'supertest';
-import app from './app.js';
+import app from './src/app.js';
 
 test('if the base route works as expected', async () => {
 	const reponse = await request(app).get('/');
-	
+
 });
