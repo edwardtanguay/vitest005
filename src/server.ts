749ca8d1afc3b2aa627dd7691533d8dacef3fb17
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-08 10:07:30 +0100

    logging removed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import * as model from './model.js';
 import express from 'express';
 import cors from 'cors';
-import logger from './logger.js';
 import * as config from './config.js';
 
 const app = express();
 app.use(cors());
-app.use(logger);
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send(model.getApiInstructions());
